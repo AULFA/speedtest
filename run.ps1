2fce96329a08352a4a9906baa300564b2498497f
Mark Raynsford <code@io7m.com>
2019-04-26 11:50:25 +0100

    UDP tests were not UDP

--- run.ps1
+++ run.ps1
@@ -26,7 +26,7 @@ function Run-TCP([Int] $Bandwidth) {
 function Run-UDP([Int] $Bandwidth) {
   $BandwidthMB = $Bandwidth * 0.000001
   Write-Output "Running $BandwidthMB MBits/sec UDP for 30 seconds"
-  ./iperf3.exe -c "${targetDomain}" --logfile "${logFile}" --format M --bandwidth $Bandwidth --time 30 --json --verbose
+  ./iperf3.exe -c "${targetDomain}" --logfile "${logFile}" --format M --bandwidth $Bandwidth --time 30 --json --verbose --udp
   if ($LASTEXITCODE -ne 0) {
     Write-Output "Test failed."
     Write-Output "See: https://github.com/AULFA/speedtest#one-or-more-tests-failed"
