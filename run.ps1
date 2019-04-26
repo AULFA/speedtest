a286ddc5e25e3a07c3f0d0e624aab2dfaae8fdb0
Mark Raynsford <code@io7m.com>
2019-04-26 11:29:20 +0100

    Add newlines for readability

--- run.ps1
+++ run.ps1
@@ -11,6 +11,7 @@ Start-Transcript -path "${scriptLogFile}"
 Write-Output "Executing speed test for ${targetDomain}"
 Write-Output "Log file is ${logFile}"
 Write-Output "Transcript is ${scriptLogFile}"
+Write-Output ""
 
 function Run-TCP([Int] $Bandwidth) {
   $BandwidthMB = $Bandwidth * 0.000001
@@ -30,6 +31,7 @@ function Run-UDP([Int] $Bandwidth) {
     Write-Output "Test failed."
     Write-Output "See: https://github.com/AULFA/speedtest#one-or-more-tests-failed"
   }
+  Write-Output ""
 }
 
 $tcpRates = @(1000, 2000, 10000, 20000, 100000, 200000, 1000000, 2000000, 10000000, 20000000, 100000000, 200000000)
@@ -38,6 +40,7 @@ $udpRates = @(1000, 2000, 10000, 20000, 100000, 200000, 1000000, 2000000, 100000
 $timeExpected = ($tcpRates.Count + $udpRates.Count) * 0.5
 
 Write-Output "Tests will be completed in approximately ${timeExpected} minutes"
+Write-Output ""
 
 foreach ($rate in $tcpRates) {
   Run-TCP $rate
